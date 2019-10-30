ba34b7cf01a621f7f79e47211c4ed0d2adc70e10
boost <maypojr@gmail.com>
2019-10-30 00:33:54 -0400

    able to modify state, complete, delete, undo

--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -4,23 +4,23 @@ import './index.css'
 const dummy = [
   {
       id: 1,
-      title: 'React is cool',
-      body: 'React workflow is fun',
+      title: 'Mix with API',
+      body: 'Build powerful apps',
       date: '1/1/1970',
       completed: false,
       expired: false
   },
   {
       id: 2,
-      title: 'React is cool',
-      body: 'React workflow is fun',
+      title: 'Learn state management',
+      body: 'making changes',
       date: '1/1/1970',
       completed: false,
       expired: false
   },
   {
       id: 3,
-      title: 'React is cool',
+      title: 'Learn React',
       body: 'React workflow is fun',
       date: '1/1/1970',
       completed: false,
@@ -45,7 +45,7 @@ const dummy = [
   {
       id: 6,
       title: 'Give this a try',
-      body: 'React workflow is fun',
+      body: 'try to undo me',
       date: '1/1/1970',
       completed: true,
       expired: false
@@ -85,43 +85,103 @@ class TodoList extends Component {
       };
 
       this.onCompleted = this.onCompleted.bind(this);
+      this.onUndo = this.onUndo.bind(this);
+      this.onDismiss = this.onDismiss.bind(this);
+      this.onDelete = this.onDelete.bind(this);
   }
 
-  onCompleted(completed) {
-    const updatedList = this.state.list.filter(function isNotID(item) {
-      return item.id !== completed;
+  onCompleted(id) {
+    const markComplete = this.state.dummy.filter(function(item) {
+      return item.id === id
     })
+    markComplete[0].completed = true
+
+    const updateList = this.state.dummy.filter(function(item) {
+      return item.id !== id
+    })
+
+    var joined = updateList.concat(markComplete)
+
+    this.setState({ dummy: joined })
+  }
+
+  onUndo(id) {
+    const markUndo = this.state.dummy.filter(function(item) {
+      return item.id === id
+    })
+    markUndo[0].completed = false
+    markUndo[0].expired = false
+
+    const updateList = this.state.dummy.filter(function(item) {
+      return item.id !== id
+    })
+
+    var joined = updateList.concat(markUndo)
+
+    this.setState({ dummy: joined })
+  }
+
+  onDismiss(id) {
+    const markDismiss = this.state.dummy.filter(function(item) {
+      return item.id === id
+    })
+    markDismiss[0].expired = true
+    markDismiss[0].completed = false
+
+    const updateList = this.state.dummy.filter(function(item) {
+      return item.id !== id
+    })
+
+    var joined = updateList.concat(markDismiss)
+
+    this.setState({ dummy: joined })
+  }
+
+  onDelete(id) {
+    const markDelete = this.state.dummy.filter(function(item) {
+      return item.id === id
+    })
+
+    const updateList = this.state.dummy.filter(function(item) {
+      return item.id !== id
+    })
+
+    this.setState({ dummy: updateList })
   }
 
   render() {
 
-    function isLive(comp) {
+    function isLive() {
       return function (item) {
         if (item.completed === false &&
             item.expired === false) {
               return item
-            }
+        } 
       }
     }
 
-    function isCompleted(comp) {
+    function isCompleted() {
       return function (item) {
         return item.completed === true
       }
     }
 
-    function isDeleted(comp) {
+    function isDeleted() {
       return function (item) {
         return item.expired === true
       }
     }
 
+    const completedList = this.state.dummy.filter(isLive(this.state.dummy))
+    const noneCompleted = this.state.dummy.filter(isCompleted(this.state.dummy))
+    const emptyList = this.state.dummy.filter(isDeleted(this.state.dummy))
+
     return(
       <div className="todo-container">
         <div className="todos-list">
-          <h1 className="head-title">My Todos</h1>
+          <h1 className="head-title">Yet Another Todo List</h1>
           <h3>Todo</h3>
-          {this.state.dummy.filter(isLive(this.state.dummy)).map(item => (
+          {completedList.length ? completedList.map(item => (
           <div key={item.id} className="todo-item">
             <div className="todo-contents">
               <div className="todo-title">{item.title}</div>
@@ -129,9 +189,12 @@ class TodoList extends Component {
               <div className="todo-date">{item.date}</div>
             </div>
             <div className="todo-btn-container">
-            <button className="todo-complete-btn">Done</button>
-            <button className="todo-dismiss-btn">X</button></div>
-          </div>))}
+              <button className="todo-complete-btn" onClick={() => this.onCompleted(item.id)}>Done</button>
+              <button className="todo-dismiss-btn" onClick={() => this.onDismiss(item.id)} >X</button>
+            </div>
+          </div>)): 
+          <div>Looks Like you have nothing to do? Congrats!</div>}
+
           <h3>Add Todo</h3>
           <form className="search-form">
             <span>Title:</span>
@@ -140,8 +203,9 @@ class TodoList extends Component {
             <input className="search-box" type="text" placeholder="Todo..."/>
           </form>
           <button className="addtodo-btn">+ ADD TODO</button>
+
           <h3>Completed</h3>
-          {this.state.dummy.filter(isCompleted(this.state.dummy)).map(item => (
+          {noneCompleted.length ? noneCompleted.map(item => (
           <div key={item.id} className="completed-todo-item">
             <div className="todo-contents">
               <div className="todo-title">{item.title}</div>
@@ -149,11 +213,14 @@ class TodoList extends Component {
               <div className="todo-date">{item.date}</div>
             </div>
             <div className="todo-btn-container">
-            <button className="todo-undo-btn">Undo</button>
-            <button className="todo-dismiss-btn">X</button></div>
-          </div>))}
+              <button className="todo-undo-btn" onClick={() => this.onUndo(item.id)}>Undo</button>
+              <button className="todo-dismiss-btn" onClick={() => this.onDismiss(item.id)}>X</button>
+            </div>
+          </div>)):
+          <div>Looks like you have some work to do...</div>}
+
           <h3>Deleted</h3>
-          {this.state.dummy.filter(isDeleted(this.state.dummy)).map(item => (
+          {emptyList.length ? emptyList.map(item => (
           <div key={item.id} className="deleted-todo-item">
             <div className="todo-contents">
               <div className="todo-title">{item.title}</div>
@@ -161,9 +228,11 @@ class TodoList extends Component {
               <div className="todo-date">{item.date}</div>
             </div>
             <div className="todo-btn-container">
-            <button className="todo-undo-btn">Undo</button>
-            <button className="todo-dismiss-btn">X</button></div>
-          </div>))}
+              <button className="todo-undo-btn" onClick={() => this.onUndo(item.id)}>Undo</button>
+              <button className="todo-dismiss-btn" onClick={() => this.onDelete(item.id)}>X</button>
+            </div>
+          </div>)):
+          <div>Deleting items from here is permanent!</div>}
         </div>
       </div>
   )}
